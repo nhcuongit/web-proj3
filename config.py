d109670dc215769fa66a60c6dbf5fcbe6018baa8
CUONG Nguyen Hung <nhcuongit@gmail.com>
2024-07-12 00:12:49 +0700

    Init source code

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL = 'cuongnh20postgres.postgres.database.azure.com' #TODO: Update value
+    POSTGRES_USER = 'cuongnh20' #TODO: Update value
+    POSTGRES_PW = 'Pos!02071708' #TODO: Update value
+    POSTGRES_DB = 'techconfdb' #TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING = 'Endpoint=sb://cuongnh20bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9e8+WwycZJSIidl6UK0TUJxkKgIu5hboZ+ASbBiBmUk=' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS = 'info@techconf.com'
+    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
